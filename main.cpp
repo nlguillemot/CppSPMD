b3c992c1b39a36215f7cb7bad698f193804bd0a0
Nicolas Guillemot <nlguillemot@gmail.com>
2016-08-23 02:52:17 -0700

    clean up project

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <cstdio>
 
-// which version of SPMD to use
+// which version to use
 //#define SCALAR
-//#define V1
-#define V2
+#define CPPSPMD
 //#define ISPC
 
 // which test to run
 //#define SIMPLE
 #define NOISE
-
-// which instruction set to use
-//#define SSE2_I32X4
-#define AVX2_I32X8
 
 #define SPMD_NOISE_OPTIMIZATION
 
@@ -173,172 +168,8 @@ void noise(float x0, float y0, float x1, float y1,
 
 #endif // SCALAR
 
-#ifdef V1
-#ifdef SSE2_I32X4
-#include "spmd_sse2-i32x4.h"
-#endif // SSE2_I32X4
-#ifdef AVX2_I32X8
+#ifdef CPPSPMD
 #include "spmd_avx2-i32x8.h"
-#endif // AVX2_I32X8
-
-#ifdef SIMPLE
-void simple(float vin[], float vout[], 
-            int n)
-{
-    spmd_foreach(0, n, [&](const lint& index)
-    {
-        vfloat v = index[vin];
-        spmd_ifelse(v < 3.0f,
-            [&] { v = v * v; },
-        /* else */
-            [&] { v = sqrt(v); });
-        index[vout] = v;
-    });
-}
-#endif // SIMPLE
-
-#ifdef NOISE
-#define NOISE_PERM_SIZE 256
-
-static int NoisePerm[2 * NOISE_PERM_SIZE] = {
-    151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140,
-    36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120,
-    234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
-    88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168,  68, 175, 74, 165, 71, 
-    134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 
-    230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161,
-    1, 216, 80, 73, 209, 76, 132, 187, 208,  89, 18, 169, 200, 196, 135, 130, 
-    116, 188, 159, 86, 164, 100, 109, 198, 173, 186,  3, 64, 52, 217, 226, 250,
-    124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 
-    47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152,  2, 44,
-    154, 163, 70, 221, 153, 101, 155, 167,  43, 172, 9, 129, 22, 39, 253,  19, 
-    98, 108, 110, 79, 113, 224, 232, 178, 185,  112, 104, 218, 246, 97, 228, 251,
-    34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249,
-    14, 239, 107, 49, 192, 214,  31, 181, 199, 106, 157, 184, 84, 204, 176, 115,
-    121, 50, 45, 127,  4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 
-    243, 141, 128, 195, 78, 66, 215, 61, 156, 180, 151, 160, 137, 91, 90, 15,
-    131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99,
-    37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252,
-    219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 
-    136, 171, 168,  68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158,
-    231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245,
-    40, 244, 102, 143, 54,  65, 25, 63, 161,  1, 216, 80, 73, 209, 76, 132, 187,
-    208,  89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 
-    198, 173, 186,  3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118,
-    126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
-    223, 183, 170, 213, 119, 248, 152,  2, 44, 154, 163, 70, 221, 153, 101, 155, 
-    167,  43, 172, 9, 129, 22, 39, 253,  19, 98, 108, 110, 79, 113, 224, 232,
-    178, 185,  112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144,
-    12, 191, 179, 162, 241,  81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214,
-    31, 181, 199, 106, 157, 184,  84, 204, 176, 115, 121, 50, 45, 127,  4, 150,
-    254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 
-    66, 215, 61, 156, 180
-};
-
-
-inline vfloat SmoothStep(vfloat low, vfloat high, vfloat value) {
-    vfloat v = clamp((value - low) / (high - low), 0.f, 1.f);
-    return v * v * (-2.f * v  + 3.f);
-}
-
-
-inline vint Floor2Int(const vfloat& val) {
-    return (vint)floor(val);
-}
-
-
-inline vfloat Grad(const vint& x, const vint& y, const vint& z, const vfloat& dx, const vfloat& dy, const vfloat& dz) {
-    vint h = ((x[NoisePerm] + y)[NoisePerm] + z)[NoisePerm];
-    h &= 15;
-    vfloat u = spmd_ternary(h<8 || h==12 || h==13, dx, dy);
-    vfloat v = spmd_ternary(h<4 || h==12 || h==13, dy, dz);
-    return spmd_ternary(h & 1, -u, u) + spmd_ternary(h & 2, -v, v);
-}
-
-
-inline vfloat NoiseWeight(const vfloat& t) {
-    vfloat t3 = t*t*t;
-    vfloat t4 = t3*t;
-    return 6.f*t4*t - 15.f*t4 + 10.f*t3;
-}
-
-
-inline vfloat Lerp(const vfloat& t, const vfloat& low, const vfloat& high) {
-    return (1.0f - t) * low + t * high;
-}
-
-
-static vfloat Noise(const vfloat& x, const vfloat& y, const vfloat& z) {
-    // Compute noise cell coordinates and offsets
-    vint ix = Floor2Int(x), iy = Floor2Int(y), iz = Floor2Int(z);
-    vfloat dx = x - ix, dy = y - iy, dz = z - iz;
-
-    // Compute gradient weights
-    ix &= (NOISE_PERM_SIZE-1);
-    iy &= (NOISE_PERM_SIZE-1);
-    iz &= (NOISE_PERM_SIZE-1);
-    vfloat w000 = Grad(ix,   iy,   iz,   dx,   dy,   dz);
-    vfloat w100 = Grad(ix+1, iy,   iz,   dx-1, dy,   dz);
-    vfloat w010 = Grad(ix,   iy+1, iz,   dx,   dy-1, dz);
-    vfloat w110 = Grad(ix+1, iy+1, iz,   dx-1, dy-1, dz);
-    vfloat w001 = Grad(ix,   iy,   iz+1, dx,   dy,   dz-1);
-    vfloat w101 = Grad(ix+1, iy,   iz+1, dx-1, dy,   dz-1);
-    vfloat w011 = Grad(ix,   iy+1, iz+1, dx,   dy-1, dz-1);
-    vfloat w111 = Grad(ix+1, iy+1, iz+1, dx-1, dy-1, dz-1);
-
-    // Compute trilinear interpolation of weights
-    vfloat wx = NoiseWeight(dx), wy = NoiseWeight(dy), wz = NoiseWeight(dz);
-    vfloat x00 = Lerp(wx, w000, w100);
-    vfloat x10 = Lerp(wx, w010, w110);
-    vfloat x01 = Lerp(wx, w001, w101);
-    vfloat x11 = Lerp(wx, w011, w111);
-    vfloat y0 = Lerp(wy, x00, x10);
-    vfloat y1 = Lerp(wy, x01, x11);
-    return Lerp(wz, y0, y1);
-}
-
-
-static vfloat Turbulence(const vfloat& x, const vfloat& y, const vfloat& z, int octaves) {
-    float omega = 0.6f;
-
-    vfloat sum = 0.0f, lambda = 1.0f, o = 1.0f;
-    for (int i = 0; i < octaves; ++i) {
-        sum += abs(o * Noise(lambda * x, lambda * y, lambda * z));
-        lambda *= 1.99f;
-        o *= omega;
-    }
-    return sum * 0.5f;
-}
-
-
-void noise(float x0, float y0, float x1, 
-           float y1, int width, int height, 
-           float output[])
-{
-    float dx = (x1 - x0) / width;
-    float dy = (y1 - y0) / height;
-
-    for (int j = 0; j < height; j++) {
-        for (int i = 0; i < width; i += programCount) {
-            vfloat x = x0 + (i + programIndex) * dx;
-            vfloat y = y0 + j * dy;
-
-            lint index = (j * width + i + programIndex);
-            index[output] = Turbulence(x, y, 0.6f, 8);
-        }
-    }
-}
-#endif // NOISE
-
-#endif // V1
-
-#ifdef V2
-#ifdef SSE2_I32X4
-#include "spmdv2_sse2-i32x4.h"
-#endif // SSE2_I32X4
-#ifdef AVX2_I32X8
-#include "spmdv2_avx2-i32x8.h"
-#endif // AVX2_I32X8
 
 #ifdef SIMPLE
 struct simple : spmd_kernel
@@ -555,20 +386,16 @@ struct noise : spmd_kernel
 };
 #endif // NOISE
 
-#endif // V2
+#endif // CPPSPMD
 
 #ifdef ISPC
 
+#ifdef SIMPLE
+#include "simple.ispc.h"
+#endif // SIMPLE
+
 #ifdef NOISE
 #include "noise.ispc.h"
-
-void noise(float x0, float y0, float x1,
-           float y1, int width, int height,
-           float output[])
-{
-    ispc::noise_ispc(x0, y0, x1, y1, width, height, output);
-}
-
 #endif // NOISE
 
 #endif // ISPC
@@ -580,13 +407,17 @@ int main()
     for (int i = 0; i < 16; ++i)
         vin[i] = (float)i;
 
-#ifdef V1
+#ifdef SCALAR
     simple(vin, vout, 16);
-#endif // V1
+#endif // SCALAR
 
-#ifdef V2
+#ifdef CPPSPMD
     spmd_call<simple>(vin, vout, 16);
-#endif // V2
+#endif // CPPSPMD
+
+#ifdef ISPC
+    ispc::simple(vin, vout, 16);
+#endif // ISPC
 
     for (int i = 0; i < 16; ++i)
         printf("%d: simple(%f) = %f\n", i, vin[i], vout[i]);
@@ -630,16 +461,12 @@ int main()
         noise(x0, y0, x1, y1, width, height, buf);
 #endif
 
-#ifdef V1
-        noise(x0, y0, x1, y1, width, height, buf);
-#endif // V1
-
-#ifdef V2
+#ifdef CPPSPMD
         spmd_call<noise>(x0, y0, x1, y1, width, height, buf);
-#endif // V2
+#endif // CPPSPMD
 
 #ifdef ISPC
-        noise(x0, y0, x1, y1, width, height, buf);
+        ispc::noise(x0, y0, x1, y1, width, height, buf);
 #endif // ISPC
     }
 
--- spmd_avx2-i32x8.h
+++ spmd_avx2-i32x8.h
@@ -2,200 +2,451 @@
 
 #include <immintrin.h>
 
+#include <cstdint>
 #include <cassert>
+#include <utility>
 
-struct exec_t
+struct spmd_kernel
 {
-    __m256 _mask;
-};
+    struct vint;
 
-exec_t operator&(const exec_t& a, const exec_t& b)
-{
-    return exec_t{ _mm256_and_ps(a._mask, b._mask) };
-}
-
-exec_t operator~(const exec_t& a)
-{
-    return exec_t{ _mm256_cmp_ps(a._mask, _mm256_setzero_ps(), _CMP_EQ_OQ) };
-}
-
-static exec_t exec = exec_t{ _mm256_cmp_ps(_mm256_setzero_ps(), _mm256_setzero_ps(), _CMP_EQ_OQ) };
-
-struct vbool
-{
-    __m256 _value;
-};
-
-struct vfloat
-{
-    __m256 _value;
-
-    vfloat& operator=(const vfloat& other)
+    struct exec_t
     {
-        _value = _mm256_blendv_ps(_value, other._value, exec._mask);
-        return *this;
+        __m256 _mask;
+    };
+
+    exec_t exec = exec_t{ _mm256_cmp_ps(_mm256_setzero_ps(), _mm256_setzero_ps(), _CMP_EQ_OQ) };
+
+    struct vbool
+    {
+        __m256 _value;
+
+    private:
+        // assignment must be masked
+        vbool& operator=(const vbool&);
+    };
+
+    vbool& store(vbool& dst, const vbool& src)
+    {
+        dst._value = _mm256_blendv_ps(dst._value, src._value, exec._mask);
+        return dst;
     }
-};
 
-vfloat operator*(const vfloat& a, const vfloat& b)
-{
-    return vfloat{ _mm256_mul_ps(a._value, b._value) };
-}
+    struct vfloat
+    {
+        __m256 _value;
 
-vfloat sqrt(const vfloat& a)
-{
-    return vfloat{ _mm256_sqrt_ps(a._value) };
-}
+        vfloat()
+            : _value(_mm256_setzero_ps())
+        { }
 
-vbool operator<(const vfloat& a, float b)
-{
-    return vbool{ _mm256_cmp_ps(a._value, _mm256_set1_ps(b), _CMP_LT_OQ) };
-}
+        explicit vfloat(const __m256& v)
+            : _value(v)
+        { }
 
-// reference to a vfloat stored linearly in memory
-struct vfloat_lref
-{
-    float* _value;
+        vfloat(float value)
+            : _value(_mm256_set1_ps(value))
+        { }
+
+        vfloat(int value)
+            : _value(_mm256_set1_ps((float)value))
+        { }
+
+    private:
+        // assignment must be masked
+        vfloat& operator=(const vfloat&);
+    };
+
+    vfloat& store(vfloat& dst, const vfloat& src)
+    {
+        dst._value = _mm256_blendv_ps(dst._value, src._value, exec._mask);
+        return dst;
+    }
+
+    // reference to a vfloat stored linearly in memory
+    struct vfloat_lref
+    {
+        float* _value;
+
+    private:
+        // ref-ref assignment must be masked both ways
+        vfloat_lref& operator=(const vfloat_lref&);
+    };
 
     // scatter
-    vfloat_lref& operator=(const vfloat& other)
+    vfloat_lref& store(vfloat_lref& dst, const vfloat& src)
     {
         int mask = _mm256_movemask_ps(exec._mask);
         if (mask == 0b11111111)
         {
             // "all on" optimization: vector store
-            _mm256_store_ps(_value, other._value);
+            _mm256_storeu_ps(dst._value, src._value);
         }
         else
         {
             // masked store
-            _mm256_maskstore_ps(_value, _mm256_castps_si256(exec._mask), other._value);
+            _mm256_maskstore_ps(dst._value, _mm256_castps_si256(exec._mask), src._value);
         }
-        return *this;
+        return dst;
     }
 
     // gather
-    operator vfloat() const
+    vfloat load(const vfloat_lref& src)
     {
         int mask = _mm256_movemask_ps(exec._mask);
         if (mask == 0b11111111)
         {
             // "all on" optimization: vector load
-            return vfloat{ _mm256_load_ps(_value) };
+            return vfloat{ _mm256_loadu_ps(src._value) };
         }
         else
         {
             // masked load
-            return vfloat{ _mm256_maskload_ps(_value, _mm256_castps_si256(exec._mask)) };
+            return vfloat{ _mm256_maskload_ps(src._value, _mm256_castps_si256(exec._mask)) };
         }
     }
-};
 
-struct lint
-{
-    __m256i _value;
-
-    vfloat_lref operator[](float* ptr) const
+    // reference to a vint stored randomly in memory
+    struct vint_vref
     {
-        return vfloat_lref{ ptr + _mm_cvtsi128_si32(_mm256_extracti128_si256(_value, 0)) };
-    }
-};
+        int* _value;
+        __m256i _vindex;
 
-lint operator+(const lint& a, int b)
-{
-    return lint{ _mm256_add_epi32(a._value, _mm256_set1_epi32(b)) };
-}
+    private:
+        // ref-ref assignment must be masked both ways
+        vint_vref& operator=(const vint_vref&);
+    };
 
-lint operator+(int a, const lint& b)
-{
-    return lint{ _mm256_add_epi32(_mm256_set1_epi32(a), b._value) };
-}
-
-static const lint programIndex = lint{ _mm256_set_epi32(7,6,5,4,3,2,1,0) };
-static const int programCount = 8;
-
-template<class IfBody>
-void spmd_if(const vbool& cond, const IfBody& ifBody)
-{
-    // save old execution mask
-    exec_t old_exec = exec;
-
-    // apply "if" mask
-    exec = exec & exec_t{ cond._value };
-
-    // "all off" optimization
-    int mask = _mm256_movemask_ps(exec._mask);
-    if (mask != 0)
+    struct vint
     {
-        ifBody();
-    }
+        __m256i _value;
 
-    // restore execution mask
-    exec = old_exec;
-}
+        vint()
+            : _value(_mm256_setzero_si256())
+        { }
 
-template<class IfBody, class ElseBody>
-void spmd_ifelse(const vbool& cond, const IfBody& ifBody, const ElseBody& elseBody)
-{
-    // save old execution mask
-    exec_t old_exec = exec;
+        explicit vint(const __m256i& value)
+            : _value(value)
+        { }
 
-    // apply "if" mask
-    exec = exec & exec_t{ cond._value };
+        vint(int value)
+            : _value(_mm256_set1_epi32(value))
+        { }
 
-    // "all off" optimization
-    int mask = _mm256_movemask_ps(exec._mask);
-    if (mask != 0)
+        vint(const vfloat& other)
+            : _value(_mm256_cvtps_epi32(other._value))
+        { }
+
+        operator vbool() const
+        {
+            return vbool{ _mm256_castsi256_ps(
+                _mm256_xor_si256(
+                    _mm256_cmpeq_epi32(_mm256_setzero_si256(), _mm256_setzero_si256()),
+                    _mm256_cmpeq_epi32(_value, _mm256_setzero_si256()))) };
+        }
+
+        operator vfloat() const
+        {
+            return vfloat{ _mm256_cvtepi32_ps(_value) };
+        }
+
+        vint_vref operator[](int* ptr) const
+        {
+            return vint_vref{ ptr, _value };
+        }
+
+    private:
+        // assignment must be masked
+        vint& operator=(const vint&);
+    };
+
+    vint& store(vint& dst, const vint& src)
     {
-        ifBody();
-    }
-
-    // invert mask for "else"
-    exec = ~exec & old_exec;
-
-    // "all off" optimization
-    mask = _mm256_movemask_ps(exec._mask);
-    if (mask != 0)
-    {
-        elseBody();
+        dst._value = _mm256_castps_si256(_mm256_blendv_ps(_mm256_castsi256_ps(dst._value), _mm256_castsi256_ps(src._value), exec._mask));
+        return dst;
     }
 
-    // restore execution mask
-    exec = old_exec;
-}
-
-template<class ForeachBody>
-void spmd_foreach(int first, int last, const ForeachBody& foreachBody)
-{
-    // could allow this, just too lazy right now.
-    assert(first <= last);
-
-    // number of loops that don't require extra masking
-    int numFullLoops = ((last - first) / programCount) * programCount;
-    // number of loops that require extra masking (if loop count is not a multiple of programCount)
-    int numPartialLoops = (last - first) % programCount;
-
-    // do every loop that doesn't need to be masked
-    __m256i loopIndex = _mm256_add_epi32(programIndex._value, _mm256_set1_epi32(first));
-    for (int i = 0; i < numFullLoops; i += programCount)
+    // scatter
+    vint_vref& store(vint_vref& dst, const vint& src)
     {
-        foreachBody(lint{ loopIndex });
-        loopIndex = _mm256_add_epi32(loopIndex, _mm256_set1_epi32(programCount));
+        __declspec(align(32)) int vindex[8];
+        _mm256_store_si256((__m256i*)vindex, dst._vindex);
+
+        __declspec(align(32)) int stored[8];
+        _mm256_store_si256((__m256i*)stored, src._value);
+
+        int mask = _mm256_movemask_ps(exec._mask);
+        for (int i = 0; i < 8; i++)
+        {
+            if (mask & (1 << i))
+                dst._value[vindex[i]] = stored[i];
+        }
+        return dst;
     }
 
-    // do a partial loop if necessary (if loop count is not a multiple of programCount)
-    if (numPartialLoops > 0)
+    // gather
+    vint load(const vint_vref& src)
+    {
+        return vint{ _mm256_mask_i32gather_epi32(_mm256_setzero_si256(), src._value, src._vindex, _mm256_castps_si256(exec._mask), 4) };
+    }
+
+    struct lint
+    {
+        __m256i _value;
+
+        explicit lint(__m256i value)
+            : _value(value)
+        { }
+
+        operator vfloat() const
+        {
+            return vfloat{ _mm256_cvtepi32_ps(_value) };
+        }
+
+        vfloat_lref operator[](float* ptr) const
+        {
+            return vfloat_lref{ ptr + _mm_cvtsi128_si32(_mm256_extracti128_si256(_value, 0)) };
+        }
+
+    private:
+        // masked assignment can produce a non-linear value
+        lint& operator=(const lint&);
+    };
+
+    const lint programIndex = lint{ _mm256_set_epi32(7,6,5,4,3,2,1,0) };
+    const int programCount = 8;
+
+    template<class IfBody>
+    void spmd_if(const vbool& cond, const IfBody& ifBody)
     {
         // save old execution mask
         exec_t old_exec = exec;
 
-        // apply mask for partial loop
-        exec = exec & exec_t{ _mm256_castsi256_ps(_mm256_cmpgt_epi32(_mm256_set1_epi32(numPartialLoops), programIndex._value)) };
+        // apply "if" mask
+        exec = exec & exec_t{ cond._value };
 
-        // do the partial loop
-        foreachBody(lint{ loopIndex });
+        // "all off" optimization
+        int mask = _mm256_movemask_ps(exec._mask);
+        if (mask != 0)
+        {
+            ifBody();
+        }
 
         // restore execution mask
         exec = old_exec;
     }
+
+    template<class IfBody, class ElseBody>
+    void spmd_ifelse(const vbool& cond, const IfBody& ifBody, const ElseBody& elseBody)
+    {
+        // save old execution mask
+        exec_t old_exec = exec;
+
+        // apply "if" mask
+        exec = exec & exec_t{ cond._value };
+
+        // "all off" optimization
+        int mask = _mm256_movemask_ps(exec._mask);
+        if (mask != 0)
+        {
+            ifBody();
+        }
+
+        // invert mask for "else"
+        exec = andnot(exec, old_exec);
+
+        // "all off" optimization
+        mask = _mm256_movemask_ps(exec._mask);
+        if (mask != 0)
+        {
+            elseBody();
+        }
+
+        // restore execution mask
+        exec = old_exec;
+    }
+
+    template<class ForeachBody>
+    void spmd_foreach(int first, int last, const ForeachBody& foreachBody)
+    {
+        // could allow this, just too lazy right now.
+        assert(first <= last);
+
+        // number of loops that don't require extra masking
+        int numFullLoops = ((last - first) / programCount) * programCount;
+        // number of loops that require extra masking (if loop count is not a multiple of programCount)
+        int numPartialLoops = (last - first) % programCount;
+
+        // do every loop that doesn't need to be masked
+        __m256i loopIndex = _mm256_add_epi32(programIndex._value, _mm256_set1_epi32(first));
+        for (int i = 0; i < numFullLoops; i += programCount)
+        {
+            foreachBody(lint{ loopIndex });
+            loopIndex = _mm256_add_epi32(loopIndex, _mm256_set1_epi32(programCount));
+        }
+
+        // do a partial loop if necessary (if loop count is not a multiple of programCount)
+        if (numPartialLoops > 0)
+        {
+            // save old execution mask
+            exec_t old_exec = exec;
+
+            // apply mask for partial loop
+            exec = exec & exec_t{ _mm256_castsi256_ps(_mm256_cmpgt_epi32(_mm256_set1_epi32(numPartialLoops), programIndex._value)) };
+
+            // do the partial loop
+            foreachBody(lint{ loopIndex });
+
+            // restore execution mask
+            exec = old_exec;
+        }
+    }
+
+    template<class SPMDKernel, class... Args>
+    auto spmd_call(Args&&... args)
+    {
+        SPMDKernel kernel;
+        kernel.exec = exec;
+        return kernel._call(std::forward<Args>(args)...);
+    }
+};
+
+spmd_kernel::exec_t operator&(const spmd_kernel::exec_t& a, const spmd_kernel::exec_t& b)
+{
+    return spmd_kernel::exec_t{ _mm256_and_ps(a._mask, b._mask) };
+}
+
+spmd_kernel::exec_t andnot(const spmd_kernel::exec_t& a, const spmd_kernel::exec_t& b)
+{
+    return spmd_kernel::exec_t{ _mm256_andnot_ps(a._mask, b._mask) };
+}
+
+spmd_kernel::vbool operator||(const spmd_kernel::vbool& a, const spmd_kernel::vbool& b)
+{
+    return spmd_kernel::vbool{ _mm256_or_ps(a._value, b._value) };
+}
+
+spmd_kernel::vfloat operator*(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vfloat{ _mm256_mul_ps(a._value, b._value) };
+}
+
+spmd_kernel::vfloat operator/(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vfloat{ _mm256_div_ps(a._value, b._value) };
+}
+
+spmd_kernel::vfloat operator-(const spmd_kernel::vfloat& v)
+{
+    return spmd_kernel::vfloat{ _mm256_sub_ps(_mm256_xor_ps(v._value, v._value), v._value) };
+}
+
+spmd_kernel::vbool operator==(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vbool{ _mm256_cmp_ps(a._value, b._value, _CMP_EQ_OQ) };
+}
+
+spmd_kernel::vbool operator<(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vbool{ _mm256_cmp_ps(a._value, b._value, _CMP_LT_OQ) };
+}
+
+spmd_kernel::vfloat spmd_ternary(const spmd_kernel::vbool& cond, const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vfloat{ _mm256_blendv_ps(b._value, a._value, cond._value) };
+}
+
+spmd_kernel::vfloat sqrt(const spmd_kernel::vfloat& v)
+{
+    return spmd_kernel::vfloat{ _mm256_sqrt_ps(v._value) };
+}
+
+spmd_kernel::vfloat abs(const spmd_kernel::vfloat& v)
+{
+    return spmd_kernel::vfloat{ _mm256_andnot_ps(_mm256_set1_ps(-0.0f), v._value) };
+}
+
+spmd_kernel::vfloat floor(const spmd_kernel::vfloat& v)
+{
+    return spmd_kernel::vfloat{ _mm256_floor_ps(v._value) };
+}
+
+spmd_kernel::vfloat clamp(const spmd_kernel::vfloat& v, const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    __m256 lomask = _mm256_cmp_ps(v._value, a._value, _CMP_LT_OQ);
+    __m256 himask = _mm256_cmp_ps(v._value, b._value, _CMP_GT_OQ);
+    __m256 okmask = _mm256_andnot_ps(_mm256_or_ps(lomask, himask), _mm256_cmp_ps(_mm256_setzero_ps(), _mm256_setzero_ps(), _CMP_EQ_OQ));
+    return spmd_kernel::vfloat{ _mm256_or_ps(_mm256_and_ps(okmask, v._value), _mm256_or_ps(_mm256_and_ps(lomask, a._value), _mm256_and_ps(himask, b._value))) };
+}
+
+spmd_kernel::vfloat fma(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b, const spmd_kernel::vfloat& c)
+{
+    return spmd_kernel::vfloat{ _mm256_fmadd_ps(a._value, b._value, c._value) };
+}
+
+spmd_kernel::vfloat fms(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b, const spmd_kernel::vfloat& c)
+{
+    return spmd_kernel::vfloat{ _mm256_fmsub_ps(a._value, b._value, c._value) };
+}
+
+spmd_kernel::vfloat fnma(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b, const spmd_kernel::vfloat& c)
+{
+    return spmd_kernel::vfloat{ _mm256_fnmadd_ps(a._value, b._value, c._value) };
+}
+
+spmd_kernel::vfloat fnms(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b, const spmd_kernel::vfloat& c)
+{
+    return spmd_kernel::vfloat{ _mm256_fnmsub_ps(a._value, b._value, c._value) };
+}
+
+spmd_kernel::lint operator+(int a, const spmd_kernel::lint& b)
+{
+    return spmd_kernel::lint{ _mm256_add_epi32(_mm256_set1_epi32(a), b._value) };
+}
+
+spmd_kernel::vfloat operator+(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vfloat{ _mm256_add_ps(a._value, b._value) };
+}
+
+spmd_kernel::vfloat operator-(const spmd_kernel::vfloat& a, const spmd_kernel::vfloat& b)
+{
+    return spmd_kernel::vfloat{ _mm256_sub_ps(a._value, b._value) };
+}
+
+spmd_kernel::vint operator&(const spmd_kernel::vint& a, const spmd_kernel::vint& b)
+{
+    return spmd_kernel::vint{ _mm256_and_si256(a._value, b._value) };
+}
+
+spmd_kernel::vbool operator==(const spmd_kernel::vint& a, const spmd_kernel::vint& b)
+{
+    return spmd_kernel::vbool{ _mm256_castsi256_ps(_mm256_cmpeq_epi32(a._value, b._value)) };
+}
+
+spmd_kernel::vbool operator<(const spmd_kernel::vint& a, const spmd_kernel::vint& b)
+{
+    return spmd_kernel::vbool{ _mm256_castsi256_ps(_mm256_cmpgt_epi32(b._value, a._value)) };
+}
+
+spmd_kernel::vint operator+(const spmd_kernel::vint& a, const spmd_kernel::vint& b)
+{
+    return spmd_kernel::vint{ _mm256_add_epi32(a._value, b._value) };
+}
+
+spmd_kernel::vbool operator==(const spmd_kernel::lint& a, const spmd_kernel::lint& b)
+{
+    return spmd_kernel::vbool{ _mm256_castsi256_ps(_mm256_cmpeq_epi32(a._value, b._value)) };
+}
+
+spmd_kernel::vbool operator<(const spmd_kernel::lint& a, const spmd_kernel::lint& b)
+{
+    return spmd_kernel::vbool{ _mm256_castsi256_ps(_mm256_cmpgt_epi32(b._value, a._value)) };
+}
+
+template<class SPMDKernel, class... Args>
+auto spmd_call(Args&&... args)
+{
+    SPMDKernel kernel;
+    return kernel._call(std::forward<Args>(args)...);
 }
