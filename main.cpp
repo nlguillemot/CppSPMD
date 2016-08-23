b86e0fc6b2ea4bebef1f9f7823d05064322d557c
Nicolas Guillemot <nlguillemot@gmail.com>
2016-08-23 04:08:09 -0700

    add mandelbrot

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 
 // which test to run
 //#define SIMPLE
-#define NOISE
+//#define NOISE
+#define MANDELBROT
 
 #define SPMD_NOISE_OPTIMIZATION
 
@@ -15,6 +16,33 @@
 #include <cmath>
 
 #ifdef SIMPLE
+/*
+  Copyright (c) 2010-2011, Intel Corporation
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Intel Corporation nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+*/
+
 void simple(float vin[], float vout[], int count) {
     for (int index = 0; index < count; index++) {
         float v = vin[index];
@@ -30,6 +58,33 @@ void simple(float vin[], float vout[], int count) {
 #endif // SIMPLE
 
 #ifdef NOISE
+/*
+  Copyright (c) 2010-2011, Intel Corporation
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Intel Corporation nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+*/
+
 #define NOISE_PERM_SIZE 256
 
 static int NoisePerm[2 * NOISE_PERM_SIZE] = {
@@ -165,6 +220,70 @@ void noise(float x0, float y0, float x1, float y1,
     }
 }
 #endif // NOISE
+
+#ifdef MANDELBROT
+/*
+  Copyright (c) 2010-2011, Intel Corporation
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Intel Corporation nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+*/
+
+
+static int mandel(float c_re, float c_im, int count) {
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < count; ++i) {
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re*z_re - z_im*z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+
+    return i;
+}
+
+void mandelbrot_serial(float x0, float y0, float x1, float y1,
+                       int width, int height, int maxIterations,
+                       int output[])
+{
+    float dx = (x1 - x0) / width;
+    float dy = (y1 - y0) / height;
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; ++i) {
+            float x = x0 + i * dx;
+            float y = y0 + j * dy;
+
+            int index = (j * width + i);
+            output[index] = mandel(x, y, maxIterations);
+        }
+    }
+}
+#endif // MANDELBROT
 
 #endif // SCALAR
 
@@ -386,6 +505,62 @@ struct noise : spmd_kernel
 };
 #endif // NOISE
 
+#ifdef MANDELBROT
+struct mandel : spmd_kernel
+{
+    vint _call(const vfloat& c_re, const vfloat& c_im, const vint& count)
+    {
+        vfloat z_re = c_re, z_im = c_im;
+        vint i;
+        spmd_for([&] { store(i, 0);  }, [&] { return i < count; }, [&] { store(i, i + 1); }, [&] {
+            if (z_re * z_re + z_im * z_im > 4.0f)
+                spmd_break();
+
+            vfloat new_re = z_re*z_re - z_im*z_im;
+            vfloat new_im = 2.f * z_re * z_im;
+            spmd_unmasked([&] {
+                store(z_re, c_re + new_re);
+                store(z_im, c_im + new_im);
+            });
+        });
+
+        return i;
+    }
+};
+
+struct mandelbrot : spmd_kernel
+{
+    void _call(
+        float x0, float y0,
+        float x1, float y1,
+        int width, int height,
+        int maxIterations,
+        int output[])
+    {
+        float dx = (x1 - x0) / width;
+        float dy = (y1 - y0) / height;
+
+        for (int j = 0; j < height; j++) {
+            // Note that we'll be doing programCount computations in parallel,
+            // so increment i by that much.  This assumes that width evenly
+            // divides programCount.
+            spmd_foreach(0, width, [&](const lint& i) {
+                // Figure out the position on the complex plane to compute the
+                // number of iterations at.  Note that the x values are
+                // different across different program instances, since its
+                // initializer incorporates the value of the programIndex
+                // variable.
+                vfloat x = x0 + i * dx;
+                vfloat y = y0 + j * dy;
+
+                lint index = j * width + i;
+                store(index[output], spmd_call<mandel>(x, y, maxIterations));
+            });
+        }
+    }
+};
+#endif // MANDELBROT
+
 #endif // CPPSPMD
 
 #ifdef ISPC
@@ -397,6 +572,10 @@ struct noise : spmd_kernel
 #ifdef NOISE
 #include "noise.ispc.h"
 #endif // NOISE
+
+#ifdef MANDELBROT
+#include "mandelbrot.ispc.h"
+#endif // MANDELBROT
 
 #endif // ISPC
 
@@ -473,3 +652,50 @@ int main()
     writePPM(buf, width, height, "noise.ppm");
 }
 #endif // NOISE
+
+#ifdef MANDELBROT
+/* Write a PPM image file with the image of the Mandelbrot set */
+static void
+writePPM(int *buf, int width, int height, const char *fn) {
+    FILE *fp = fopen(fn, "wb");
+    fprintf(fp, "P6\n");
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "255\n");
+    for (int i = 0; i < width*height; ++i) {
+        // Map the iteration count to colors by just alternating between
+        // two greys.
+        char c = (buf[i] & 0x1) ? 240 : 20;
+        for (int j = 0; j < 3; ++j)
+            fputc(c, fp);
+    }
+    fclose(fp);
+    printf("Wrote image file %s\n", fn);
+}
+
+int main()
+{
+    unsigned int width = 768;
+    unsigned int height = 512;
+    float x0 = -2;
+    float x1 = 1;
+    float y0 = -1;
+    float y1 = 1;
+
+    int maxIterations = 256;
+    int *buf = new int[width*height];
+
+#ifdef SCALAR
+    mandelbrot(x0, y0, x1, y1, width, height, maxIterations, buf);
+#endif // SCALAR
+
+#ifdef CPPSPMD
+    spmd_call<mandelbrot>(x0, y0, x1, y1, width, height, maxIterations, buf);
+#endif // CPPSPMD
+
+#ifdef ISPC
+    ispc::mandelbrot(x0, y0, x1, y1, width, height, maxIterations, buf);
+#endif // ISPC
+
+    writePPM(buf, width, height, "mandelbrot.ppm");
+}
+#endif // MANDELBROT
